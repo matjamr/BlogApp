97cc264f4c88cc8c6fcde0265527e310bf97e49c
Mateusz Jamróz <jamr.mat@gmail.com>
2022-02-26 08:27:29 +0100

    Creating project

---
+++ src/test/java/com/example/testingproject/TestingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
